481158463755598952cf6c800700b177b70ee1a3
Lalit <lalitpatidar388@gmail.com>
2025-02-22 23:55:59 +0530

    Changed config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['"i.pinimg.com','www.socialpilot.co','cdn.thebrandingjournal.com'],
-    
+    domains: ['i.pinimg.com', 'www.socialpilot.co', 'cdn.thebrandingjournal.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      timers: require.resolve('timers-browserify'),
+    };
+    return config;
+  },
+  experimental: {
+    turbo: {}, // Disable Turbopack
   },
 };
 
